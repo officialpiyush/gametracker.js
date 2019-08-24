75740e735d8e4730aeabef3300a3a8a6bbdd7c2d
Piyush Bhangale <bhangalepiyush@gmail.com>
2019-08-24 23:40:37 +0530

    feat: Remove Files

--- examples/FortniteTest.js
+++
@@ -1,4 +0,0 @@
-const { FortniteClient } = require("../build/index");
-
-const client = new FortniteClient();
-client.getStats("ionagamdfgeer").then(r => console.log(r))
--- src/lib/utils/errors/InvalidFortniteUser.ts
+++
@@ -1,7 +0,0 @@
-export default class InvalidFortniteUser extends Error {
-    public constructor(name: string) {
-        super();
-        this.name = "InvalidFortniteUser";
-        this.message = `[Invalid InGamename] Got invalid InGameName: ${name}`;
-    }
-}
